4892714fac8c93d05a5971c5fe16a7573af8d3cf
faiq <faiqrazarizvi@gmail.com>
2017-01-31 10:40:23 -0800

    some djanky parsing

--- house/house.js
+++ house/house.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const xml2js = require('xml2js')
 
 BillFetcher.prototype.FetchBills = function (date, cb) {
+  //if it's a file we probably aren't too worried about
   if (typeof date == "function") {
     cb = date
   }
@@ -27,18 +28,27 @@ BillFetcher.prototype.parseData = function (data, cb) {
     }
     var a = []
     for (var i = 0; i <  result.floorschedule.category.length; i++) {
-      a.push(result.floorschedule.category[i]['floor-items'])
+      for (var j = 0; j < result.floorschedule.category[i]['floor-items'].length; j++) {
+        for (var k = 0; k < result.floorschedule.category[i]['floor-items'][j]['floor-item'].length; k++) {
+          var obj = {}
+          obj['bill-number'] = result.floorschedule.category[i]['floor-items'][j]['floor-item'][k]['legis-num'][0]
+          Object.assign(obj, result.floorschedule.category[i]['floor-items'][j]['floor-item'][k]['$'])
+          obj['bill-link'] = result.floorschedule.category[i]['floor-items'][j]['floor-item'][k]['files'][0]['file'][0]['$']['doc-url']
+          a.push(obj)
+        }
+      }
     }
     return cb(null, a)
   })
-
 }
 
 function BillFetcher(opts) {
     if (opts.File != "" && (opts.URL == "" || !opts.URL)) {
       this.File = opts.File
     }
-    console.log(this.File)
+    if (opts.URL != "" && (opts.File == "" || !opts.url)) {
+      this.URL = opts.url
+    }
     this.parser = new xml2js.Parser()
 }
 
